7d0590735177d4ae6ead23b71457384b9162503b
Nils <imdahlnils@gmail.com>
2020-11-08 17:00:42 +0100

    part2?

--- src/Exercise01.hs
+++ src/Exercise01.hs
@@ -1,6 +1,7 @@
 module Exercise01 where
 
 import Test.QuickCheck
+import Text.Printf
 
 {-H1.1a)-}
 myPair :: Integer -> Integer -> Integer
@@ -10,7 +11,7 @@ myPair x y = 2^y * (2 * x + 1) - 1
 mySnd :: Integer -> Integer
 mySnd y
     | even y = 0
-    | odd y = 1 + mySnd ((y-1) `div` 2)
+    | odd y = 1 + mySnd (y `div` 2)
 
 
 {-H1.1c)-}
@@ -20,7 +21,7 @@ myFst x = ((x+1) `div` (2 ^ mySnd x) -1) `div` 2
 {-H1.1d)-}
 prop_myPair :: Integer -> Integer -> Integer -> Property
 prop_myPair p x y = 
-    p >=0 && x>=0 && y >=0  ==> property (myPair x y == p)
+    x>=0 && y>=0  ==> property (myPair x y == p)
 
 {-H2-}
 digitToEo :: Integer -> String
@@ -38,6 +39,21 @@ digitToEo 9 = "nau"
 {-WETT-}
 
 numberToEo :: Integer -> String
-numberToEo n = undefined
+numberToEo n = 
+    let x = read (printf "%06d" n) in 
+      init (digitToEoHelper (mod (div x 100000) 10) "cent " 
+        ++ digitToEoHelper (mod (div x 10000) 10) "dek " 
+        ++ digitToEoHelper (mod (div x 1000) 10)  " "
+        ++ digitToEoHelper x "mil"
+        ++ digitToEoHelper (mod (div x 100) 10) "cent "
+        ++ digitToEoHelper (mod (div x 10) 10) "dek "
+        ++ digitToEoHelper (mod x 10) "")
+
+
+digitToEoHelper:: Integer -> String -> String
+digitToEoHelper x "mil" = if x < 1000 then "" else "mil "
+digitToEoHelper 0 s = ""
+digitToEoHelper 1 s = if s == "" || s == " " then "unu " else s
+digitToEoHelper x s = digitToEo x ++ s  
 
 {-TTEW-}
